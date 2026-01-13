e30815fcf5991a92cd7d5abb4df059cec0651a0c
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:22 -0500

    AI Expansion: src/pages/app/platform/DataNetworkPage.tsx

---
+++ src/pages/app/platform/DataNetworkPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PlaidDashboardView from '../../../components/PlaidDashboardView';
+
+const DataNetworkPage: React.FC = () => {
+  return (
+    <div>
+      <PlaidDashboardView />
+    </div>
+  );
+};
+
+export default DataNetworkPage;
