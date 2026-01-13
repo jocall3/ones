05b805c8748c6f57c8006ac362b9d615e7bcbc0b
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:53 -0500

    AI Expansion: src/pages/app/personal-finance/FinancialGoalsPage.tsx

---
+++ src/pages/app/personal-finance/FinancialGoalsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialGoalsView from '../../../components/FinancialGoalsView';
+
+const FinancialGoalsPage: React.FC = () => {
+  return (
+    <div>
+      <FinancialGoalsView />
+    </div>
+  );
+};
+
+export default FinancialGoalsPage;
