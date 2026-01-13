d496e0a66fe4459971b17487dddcd6bc06c24bce
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:21 -0500

    AI Expansion: src/pages/app/platform/AgentMarketplacePage.tsx

---
+++ src/pages/app/platform/AgentMarketplacePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketplaceView from '../../../components/AgentMarketplaceView (1).tsx';
+
+const AgentMarketplacePage: React.FC = () => {
+  return (
+    <div>
+      <MarketplaceView />
+    </div>
+  );
+};
+
+export default AgentMarketplacePage;
