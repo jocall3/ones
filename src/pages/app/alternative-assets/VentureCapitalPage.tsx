ef6a3ad00987af46ea2beeec9a659296e33d09c8
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:04 -0500

    AI Expansion: src/pages/app/alternative-assets/VentureCapitalPage.tsx

---
+++ src/pages/app/alternative-assets/VentureCapitalPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VentureCapitalDesk from '../../../components/VentureCapitalDesk';
+
+const VentureCapitalPage: React.FC = () => {
+  return (
+    <div>
+      <VentureCapitalDesk />
+    </div>
+  );
+};
+
+export default VentureCapitalPage;
