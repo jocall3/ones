3f788de578af65f122cac45156460f449d25b17e
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:29 -0500

    AI Expansion: src/pages/app/platform/ConciergeServicePage.tsx

---
+++ src/pages/app/platform/ConciergeServicePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ConciergeService from '../../../components/ConciergeService';
+
+const ConciergeServicePage: React.FC = () => {
+  return (
+    <div>
+      <ConciergeService />
+    </div>
+  );
+};
+
+export default ConciergeServicePage;
