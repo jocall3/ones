907e0325a0980d1731f4be6fccf7905f7d13eb61
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:32 -0500

    AI Expansion: src/pages/app/platform/PhilanthropyPage.tsx

---
+++ src/pages/app/platform/PhilanthropyPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhilanthropyHub from '../../../components/PhilanthropyHub.tsx';
+
+const PhilanthropyPage: React.FC = () => {
+  return (
+    <div>
+      <PhilanthropyHub />
+    </div>
+  );
+};
+
+export default PhilanthropyPage;
