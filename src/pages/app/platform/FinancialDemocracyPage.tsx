60bfcb0511c925c388a6c0e362aaf6a0a985481b
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:20 -0500

    AI Expansion: src/pages/app/platform/FinancialDemocracyPage.tsx

---
+++ src/pages/app/platform/FinancialDemocracyPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialDemocracyView from '../../../components/FinancialDemocracyView';
+
+const FinancialDemocracyPage: React.FC = () => {
+  return (
+    <div>
+      <FinancialDemocracyView />
+    </div>
+  );
+};
+
+export default FinancialDemocracyPage;
