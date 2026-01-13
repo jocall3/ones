3b512cc6a942f6669dab374700727e908f70acee
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:38 -0500

    AI Expansion: src/pages/app/ai/AIAdStudioPage.tsx

---
+++ src/pages/app/ai/AIAdStudioPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AIAdStudioView from '../../../components/AIAdStudioView';
+
+const AIAdStudioPage: React.FC = () => {
+  return (
+    <div>
+      <AIAdStudioView />
+    </div>
+  );
+};
+
+export default AIAdStudioPage;
