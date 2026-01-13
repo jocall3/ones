ad505fe2150ff2040c67210bd7b1868a59a68e29
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:47 -0500

    AI Expansion: src/pages/app/security/ComplianceOraclePage.tsx

---
+++ src/pages/app/security/ComplianceOraclePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ComplianceOracleView from '../../../components/ComplianceOracleView';
+
+const ComplianceOraclePage: React.FC = () => {
+  return (
+    <div>
+      <ComplianceOracleView />
+    </div>
+  );
+};
+
+export default ComplianceOraclePage;
