08c438d415e8ba1f1654d0cd0d15deef20f2da34
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:48 -0500

    AI Expansion: src/pages/app/payments/SendMoneyPage.tsx

---
+++ src/pages/app/payments/SendMoneyPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SendMoneyView from '../../../components/SendMoneyView';
+
+const SendMoneyPage: React.FC = () => {
+  return (
+    <div>
+      <SendMoneyView />
+    </div>
+  );
+};
+
+export default SendMoneyPage;
