e8fcd82aa69fcd77e434fdbb40fec6a3f867885c
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:49 -0500

    AI Expansion: src/pages/app/payments/BillPayPage.tsx

---
+++ src/pages/app/payments/BillPayPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CitibankBillPayView from '../../../components/CitibankBillPayView';
+
+const BillPayPage: React.FC = () => {
+  return (
+    <div>
+      <CitibankBillPayView />
+    </div>
+  );
+};
+
+export default BillPayPage;
