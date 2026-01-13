37016e3bc03741798748deca2756c04d22c03750
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:15 -0500

    AI Expansion: src/pages/app/corporate/ReconciliationPage.tsx

---
+++ src/pages/app/corporate/ReconciliationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReconciliationHubView from '../../../components/ReconciliationHubView';
+
+const ReconciliationPage: React.FC = () => {
+  return (
+    <div>
+      <ReconciliationHubView />
+    </div>
+  );
+};
+
+export default ReconciliationPage;
