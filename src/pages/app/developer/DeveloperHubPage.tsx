a5b4fea95ea0d156e78b9186e6a4b53cb42d103b
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:47 -0500

    AI Expansion: src/pages/app/developer/DeveloperHubPage.tsx

---
+++ src/pages/app/developer/DeveloperHubPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DeveloperHubView from '../../../components/DeveloperHubView';
+
+const DeveloperHubPage: React.FC = () => {
+  return (
+    <div>
+      <DeveloperHubView />
+    </div>
+  );
+};
+
+export default DeveloperHubPage;
