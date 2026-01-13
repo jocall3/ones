aa56fbc8de0a5345142b29397df6bb3326452e8d
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:46 -0500

    AI Expansion: src/pages/app/transactions/TransactionsHistoryPage.tsx

---
+++ src/pages/app/transactions/TransactionsHistoryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TransactionsView from '../../../components/TransactionsView';
+
+const TransactionsHistoryPage: React.FC = () => {
+  return (
+    <div>
+      <TransactionsView />
+    </div>
+  );
+};
+
+export default TransactionsHistoryPage;
