475dc1e671e90f740e94f76af6c8b8d43b76a87a
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:50 -0500

    AI Expansion: src/pages/app/developer/ApiPlaygroundPage.tsx

---
+++ src/pages/app/developer/ApiPlaygroundPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ApiPlaygroundView from '../../../components/ApiPlaygroundView';
+
+const ApiPlaygroundPage: React.FC = () => {
+  return (
+    <div>
+      <ApiPlaygroundView />
+    </div>
+  );
+};
+
+export default ApiPlaygroundPage;
