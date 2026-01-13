284fd2e8cf1f91dd0787ad1142c14eb813b1190b
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:59 -0500

    AI Expansion: src/pages/app/trading/CommoditiesExchangePage.tsx

---
+++ src/pages/app/trading/CommoditiesExchangePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CommoditiesExchange from '../../../components/CommoditiesExchange.tsx';
+
+const CommoditiesExchangePage: React.FC = () => {
+  return (
+    <div>
+      <CommoditiesExchange />
+    </div>
+  );
+};
+
+export default CommoditiesExchangePage;
