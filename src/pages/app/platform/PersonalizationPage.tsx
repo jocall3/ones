c18e77a47f2943b6e99c6c88f7e248ee1cad27c1
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:31 -0500

    AI Expansion: src/pages/app/platform/PersonalizationPage.tsx

---
+++ src/pages/app/platform/PersonalizationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PersonalizationView from '../../../components/PersonalizationView';
+
+const PersonalizationPage: React.FC = () => {
+  return (
+    <div>
+      <PersonalizationView />
+    </div>
+  );
+};
+
+export default PersonalizationPage;
