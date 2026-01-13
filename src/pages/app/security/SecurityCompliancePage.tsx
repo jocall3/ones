ce56853cd3182239a5098c5fd6d525fe3c6fd6ab
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:46 -0500

    AI Expansion: src/pages/app/security/SecurityCompliancePage.tsx

---
+++ src/pages/app/security/SecurityCompliancePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SecurityComplianceView from '../../../components/SecurityComplianceView';
+
+const SecurityCompliancePage: React.FC = () => {
+  return (
+    <div>
+      <SecurityComplianceView />
+    </div>
+  );
+};
+
+export default SecurityCompliancePage;
