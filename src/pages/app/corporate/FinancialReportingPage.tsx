f8e703c6b8170bb85074982df048522575ea40fd
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:16 -0500

    AI Expansion: src/pages/app/corporate/FinancialReportingPage.tsx

---
+++ src/pages/app/corporate/FinancialReportingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FinancialReportingView from '../../../components/FinancialReportingView';
+
+const FinancialReportingPage: React.FC = () => {
+  return (
+    <div>
+      <FinancialReportingView />
+    </div>
+  );
+};
+
+export default FinancialReportingPage;
