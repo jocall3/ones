38cdd60a767ce21db72079f611ad8c52c0be369f
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:42 -0500

    AI Expansion: src/pages/app/ai/QuantumWeaverPage.tsx

---
+++ src/pages/app/ai/QuantumWeaverPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import QuantumWeaverView from '../../../components/QuantumWeaverView';
+
+const QuantumWeaverPage: React.FC = () => {
+  return (
+    <div>
+      <QuantumWeaverView />
+    </div>
+  );
+};
+
+export default QuantumWeaverPage;
