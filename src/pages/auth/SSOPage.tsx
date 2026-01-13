0b3be4ed7ba9be5490ac0c472a3efcfe7741d59c
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:32:04 -0500

    AI Expansion: src/pages/auth/SSOPage.tsx

---
+++ src/pages/auth/SSOPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SSOView from '../../components/SSOView';
+
+const SSOPage: React.FC = () => {
+  return (
+    <div>
+      <SSOView />
+    </div>
+  );
+};
+
+export default SSOPage;
