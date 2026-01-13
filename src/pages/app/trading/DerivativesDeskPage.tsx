466f8340a145431352539065796b2ae189c6ccf6
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:59 -0500

    AI Expansion: src/pages/app/trading/DerivativesDeskPage.tsx

---
+++ src/pages/app/trading/DerivativesDeskPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DerivativesDesk from '../../../components/DerivativesDesk.tsx';
+
+const DerivativesDeskPage: React.FC = () => {
+  return (
+    <div>
+      <DerivativesDesk />
+    </div>
+  );
+};
+
+export default DerivativesDeskPage;
