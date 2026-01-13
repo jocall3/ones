f7e241cf1adb500233a68fbd4b6eb04caff50cdb
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:10 -0500

    AI Expansion: src/pages/app/corporate/CorporateCommandPage.tsx

---
+++ src/pages/app/corporate/CorporateCommandPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CorporateCommandView from '../../../components/CorporateCommandView';
+
+const CorporateCommandPage: React.FC = () => {
+  return (
+    <div>
+      <CorporateCommandView />
+    </div>
+  );
+};
+
+export default CorporateCommandPage;
