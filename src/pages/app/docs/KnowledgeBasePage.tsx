ce4e442ed9473de81856e3888e62fea0f15bdabb
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:58 -0500

    AI Expansion: src/pages/app/docs/KnowledgeBasePage.tsx

---
+++ src/pages/app/docs/KnowledgeBasePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import KnowledgeBaseView from '../../../components/KnowledgeBaseView';
+
+const KnowledgeBasePage: React.FC = () => {
+  return (
+    <div>
+      <KnowledgeBaseView />
+    </div>
+  );
+};
+
+export default KnowledgeBasePage;
