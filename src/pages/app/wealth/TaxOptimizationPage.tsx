d61cf3b2cf30273461ab3fd3c506d67c6f828da9
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:07 -0500

    AI Expansion: src/pages/app/wealth/TaxOptimizationPage.tsx

---
+++ src/pages/app/wealth/TaxOptimizationPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaxOptimizationChamber from '../../../components/TaxOptimizationChamber.tsx';
+
+const TaxOptimizationPage: React.FC = () => {
+  return (
+    <div>
+      <TaxOptimizationChamber />
+    </div>
+  );
+};
+
+export default TaxOptimizationPage;
