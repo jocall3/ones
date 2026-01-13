a5cc2ca1e85d111874a1a1775fab0593a37b7906
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:49 -0500

    AI Expansion: src/pages/app/developer/SchemaExplorerPage.tsx

---
+++ src/pages/app/developer/SchemaExplorerPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SchemaExplorer from '../../../components/SchemaExplorer';
+
+const SchemaExplorerPage: React.FC = () => {
+  return (
+    <div>
+      <SchemaExplorer />
+    </div>
+  );
+};
+
+export default SchemaExplorerPage;
