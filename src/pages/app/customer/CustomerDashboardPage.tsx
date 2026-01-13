70c94b53f7936ff68ad0f7a5a3aa1e63033770b0
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:35:41 -0500

    AI Expansion: src/pages/app/customer/CustomerDashboardPage.tsx

---
+++ src/pages/app/customer/CustomerDashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CustomerDashboard from '../../../components/customers/CustomerDashboard';
+
+const CustomerDashboardPage: React.FC = () => {
+  return (
+    <div>
+      <CustomerDashboard />
+    </div>
+  );
+};
+
+export default CustomerDashboardPage;
