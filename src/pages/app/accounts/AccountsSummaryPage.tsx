4246817566e4a72f276b54b0cdbffa4e3bf3a8f2
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:43 -0500

    AI Expansion: src/pages/app/accounts/AccountsSummaryPage.tsx

---
+++ src/pages/app/accounts/AccountsSummaryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AccountsDashboardView from '../../../components/AccountsDashboardView';
+
+const AccountsSummaryPage: React.FC = () => {
+  return (
+    <div>
+      <AccountsDashboardView />
+    </div>
+  );
+};
+
+export default AccountsSummaryPage;
