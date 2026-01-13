030c233bbc79aba616b49e4fcdb717c93551d70d
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:02 -0500

    AI Expansion: src/pages/app/alternative-assets/RealEstatePage.tsx

---
+++ src/pages/app/alternative-assets/RealEstatePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RealEstateEmpire from '../../../components/RealEstateEmpire.tsx';
+
+const RealEstatePage: React.FC = () => {
+  return (
+    <div>
+      <RealEstateEmpire />
+    </div>
+  );
+};
+
+export default RealEstatePage;
