4ecda3e46bc6a8785dcb17c2bd8a055e8de9955e
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:51 -0500

    AI Expansion: src/pages/app/personal-finance/BudgetsPage.tsx

---
+++ src/pages/app/personal-finance/BudgetsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BudgetsView from '../../../components/BudgetsView';
+
+const BudgetsPage: React.FC = () => {
+  return (
+    <div>
+      <BudgetsView />
+    </div>
+  );
+};
+
+export default BudgetsPage;
