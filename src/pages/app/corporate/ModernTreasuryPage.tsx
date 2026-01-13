f9d4290d8a11127d37181d41dfa202601e6f24e6
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:13 -0500

    AI Expansion: src/pages/app/corporate/ModernTreasuryPage.tsx

---
+++ src/pages/app/corporate/ModernTreasuryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ModernTreasuryView from '../../../components/ModernTreasuryView';
+
+const ModernTreasuryPage: React.FC = () => {
+  return (
+    <div>
+      <ModernTreasuryView />
+    </div>
+  );
+};
+
+export default ModernTreasuryPage;
