78f3f06cc69e5b07534a691ef07e50365c143d8a
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:56 -0500

    AI Expansion: src/pages/app/investments/CryptoWeb3Page.tsx

---
+++ src/pages/app/investments/CryptoWeb3Page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CryptoView from '../../../components/CryptoView';
+
+const CryptoWeb3Page: React.FC = () => {
+  return (
+    <div>
+      <CryptoView />
+    </div>
+  );
+};
+
+export default CryptoWeb3Page;
