34264dd81bc34f6f8013cf3b29558f0dbcb17359
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:55 -0500

    AI Expansion: src/pages/app/customer/VerificationReportsPage.tsx

---
+++ src/pages/app/customer/VerificationReportsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VerificationReportsView from '../../../components/VerificationReportsView';
+
+const VerificationReportsPage: React.FC = () => {
+  return (
+    <div>
+      <VerificationReportsView />
+    </div>
+  );
+};
+
+export default VerificationReportsPage;
