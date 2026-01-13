316b7eb1e47e6ff756c342d2afbc7d958fa4a2e8
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:54 -0500

    AI Expansion: src/pages/app/investments/PortfolioOverviewPage.tsx

---
+++ src/pages/app/investments/PortfolioOverviewPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import InvestmentsView from '../../../components/InvestmentsView';
+
+const PortfolioOverviewPage: React.FC = () => {
+  return (
+    <div>
+      <InvestmentsView />
+    </div>
+  );
+};
+
+export default PortfolioOverviewPage;
