5f3234c7a1e0a4ef557e54b9521a53bdc3a4e481
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:05 -0500

    AI Expansion: src/pages/app/alternative-assets/PrivateEquityPage.tsx

---
+++ src/pages/app/alternative-assets/PrivateEquityPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrivateEquityLounge from '../../../components/PrivateEquityLounge.tsx';
+
+const PrivateEquityPage: React.FC = () => {
+  return (
+    <div>
+      <PrivateEquityLounge />
+    </div>
+  );
+};
+
+export default PrivateEquityPage;
