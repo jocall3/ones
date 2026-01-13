b70d3167af8ea6ce9a7d60c1d996abfaaa68a22f
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:49 -0500

    AI Expansion: src/pages/app/developer/ResourceGraphPage.tsx

---
+++ src/pages/app/developer/ResourceGraphPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ResourceGraphView from '../../../components/ResourceGraphView';
+
+const ResourceGraphPage: React.FC = () => {
+  return (
+    <div>
+      <ResourceGraphView />
+    </div>
+  );
+};
+
+export default ResourceGraphPage;
