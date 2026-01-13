a66daa44e916c79d1a783219c9f986b43c8872f4
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:18 -0500

    AI Expansion: src/pages/app/platform/OpenBankingPage.tsx

---
+++ src/pages/app/platform/OpenBankingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OpenBankingView from '../../../components/OpenBankingView';
+
+const OpenBankingPage: React.FC = () => {
+  return (
+    <div>
+      <OpenBankingView />
+    </div>
+  );
+};
+
+export default OpenBankingPage;
