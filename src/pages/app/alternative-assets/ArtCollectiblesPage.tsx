0b3f0bb840efaddca69444d242166c498d2919f2
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:03 -0500

    AI Expansion: src/pages/app/alternative-assets/ArtCollectiblesPage.tsx

---
+++ src/pages/app/alternative-assets/ArtCollectiblesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArtCollectibles from '../../../components/ArtCollectibles.tsx';
+
+const ArtCollectiblesPage: React.FC = () => {
+  return (
+    <div>
+      <ArtCollectibles />
+    </div>
+  );
+};
+
+export default ArtCollectiblesPage;
