1a731508e43bf105238e9b76e8d0f81e7b1cdc33
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:50 -0500

    AI Expansion: src/pages/app/payments/CrossBorderPage.tsx

---
+++ src/pages/app/payments/CrossBorderPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CitibankCrossBorderView from '../../../components/CitibankCrossBorderView';
+
+const CrossBorderPage: React.FC = () => {
+  return (
+    <div>
+      <CitibankCrossBorderView />
+    </div>
+  );
+};
+
+export default CrossBorderPage;
