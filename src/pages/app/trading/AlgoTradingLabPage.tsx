803656cd82ef220236f7c83c96cf72cb09c6dff0
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:57 -0500

    AI Expansion: src/pages/app/trading/AlgoTradingLabPage.tsx

---
+++ src/pages/app/trading/AlgoTradingLabPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AlgoTradingLab from '../../../components/AlgoTradingLab';
+
+const AlgoTradingLabPage: React.FC = () => {
+  return (
+    <div>
+      <AlgoTradingLab />
+    </div>
+  );
+};
+
+export default AlgoTradingLabPage;
