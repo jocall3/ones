8ada34008af32bc77337319e445de53e123f363d
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:24 -0500

    AI Expansion: src/pages/app/platform/PaymentsPlatformPage.tsx

---
+++ src/pages/app/platform/PaymentsPlatformPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import StripeDashboardView from '../../../components/StripeDashboardView';
+
+const PaymentsPlatformPage: React.FC = () => {
+  return (
+    <div>
+      <StripeDashboardView />
+    </div>
+  );
+};
+
+export default PaymentsPlatformPage;
