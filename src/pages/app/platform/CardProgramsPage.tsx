323545ea1cc1f137897f286d598584e52453508c
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:26 -0500

    AI Expansion: src/pages/app/platform/CardProgramsPage.tsx

---
+++ src/pages/app/platform/CardProgramsPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MarqetaDashboardView from '../../../components/MarqetaDashboardView';
+
+const CardProgramsPage: React.FC = () => {
+  return (
+    <MarqetaDashboardView />
+  );
+};
+
+export default CardProgramsPage;
