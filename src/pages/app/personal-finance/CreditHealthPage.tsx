bc30825cdea259ce0e245f8fe243d4152bed3806
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:33:54 -0500

    AI Expansion: src/pages/app/personal-finance/CreditHealthPage.tsx

---
+++ src/pages/app/personal-finance/CreditHealthPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CreditHealthView from '../../../components/CreditHealthView';
+
+const CreditHealthPage: React.FC = () => {
+  return (
+    <div>
+      <CreditHealthView />
+    </div>
+  );
+};
+
+export default CreditHealthPage;
