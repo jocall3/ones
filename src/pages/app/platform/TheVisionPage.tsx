65fcfefcb0494dddbb47d652db0eb9872d267a3e
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:37 -0500

    AI Expansion: src/pages/app/platform/TheVisionPage.tsx

---
+++ src/pages/app/platform/TheVisionPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TheVisionView from '../../../components/TheVisionView';
+
+const TheVisionPage: React.FC = () => {
+  return (
+    <div>
+      <TheVisionView />
+    </div>
+  );
+};
+
+export default TheVisionPage;
