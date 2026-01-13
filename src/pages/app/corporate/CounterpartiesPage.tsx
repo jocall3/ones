3fd410f9756ca7ae4ccec555bcc70cc9d358338f
James Burvel O’Callaghan III <dev.citibankdemobusiness.app@gmail.com>
2026-01-13 16:34:13 -0500

    AI Expansion: src/pages/app/corporate/CounterpartiesPage.tsx

---
+++ src/pages/app/corporate/CounterpartiesPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterpartyDashboardView from '../../../components/CounterpartyDashboardView';
+
+const CounterpartiesPage: React.FC = () => {
+  return (
+    <div>
+      <CounterpartyDashboardView />
+    </div>
+  );
+};
+
+export default CounterpartiesPage;
